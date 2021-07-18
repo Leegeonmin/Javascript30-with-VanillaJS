4b8dfbe079c7eb9702dd32eda36288953ea119bc
LeeGeonMin <kmlee97s@gmail.com>
2021-07-18 14:07:43 +0900

    succeed in implementation

--- Day3_Cssvar/var.js
+++ Day3_Cssvar/var.js
@@ -3,10 +3,10 @@ const blur = document.getElementById("blur");
 const base = document.getElementById("base");
 const img = document.getElementById("img");
 
-const div = document.createElement("div");
+/* const div = document.createElement("div");
 const parentdiv = document.createElement("div");
 
-/*parentdiv.style.justifyContent = "center";
+parentdiv.style.justifyContent = "center";
 parentdiv.style.display = "flex";
 div.style.backgroundColor = `${base.value}`;
 div.style.width = "800px";
@@ -16,13 +16,21 @@ img.style.position = "relative";*/
 
 window.addEventListener("input", changecss);
 
+
+img.style.padding = "20px";
+img.style.filter = "blur(10px)";
+img.style.backgroundColor = `${base.value}`;
+
 function changecss(e){
-    console.log(e.target.value, e.target.id);
-    console.log()
-    img.style.backgroundColor = `${base.value}`;
-    img.style.padding = "10px";
+    if(e.target.id === "spacing"){
+        img.style.padding = `${spacing.value}px`;
+    }else if(e.target.id === "blur"){
+        img.style.filter = `blur(${blur.value}px)`;
+    }else{
+        img.style.backgroundColor = `${base.value}`;
+    }
 }
-console.log(`${spacing.value} : ${blur.value} : ${base.value}`);
+
 
 
 /*document.body.appendChild(parentdiv);
